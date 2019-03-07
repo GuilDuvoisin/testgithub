6814d5152d6eb5e5420dcffa5e0025045ac76695
DUVOISIN Guillaume <Guillaume.DUVOISIN@cpnv.ch>
2019-03-07 10:07:10 +0100

    add comment

--- main.c
+++ main.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 
-int main()
-
+int()
 {
-
     int i, nombre, resultat;
-
         printf("\nEntrer un nombre : ");
-
-        scanf("%d",&nombre);
-
+            scanf("%d",&nombre);
         resultat = 1;
+            for (i=2;i<=nombre;i++)
+            {
+                resultat *=i;
+            }
+            printf("\n\nLa factorielle de %d est : %d \n ",nombre,resultat);
 
-        for (i=2;i<=nombre;i++)
-
-        {
-            resultat = resultat*i;
-        }
-
-        printf("\n\nLa factorielle de %d est : %d \n ",nombre,resultat);
 }
